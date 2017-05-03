baf14a5ec80a84ffa75910f5bde8b002329432fb
prajwal acharya <callmeprajwal@gmail.com>
2017-05-03 15:03:38 -0700

    Updated JS

--- Appointment/mysite/appointment/static/appointment/appointment.js
+++ Appointment/mysite/appointment/static/appointment/appointment.js
@@ -5,12 +5,12 @@ $(document).ready(function () {
     var getAppointments = function (myarg) {
         $("#output").empty();
         $.ajax({
-            url: 'appointment/ajax/',
-            data: {
+            url: 'appointment/ajax/'
+            , data: {
                 'txtval': myarg
-            },
-            dataType: 'json',
-            success: function (data) {
+            }
+            , dataType: 'json'
+            , success: function (data) {
                 var mydat = data.test;
                 var str = "";
                 str = str + "<table style=\"width:100%\"> <tr> <th>Date</th> <th>Time</th> <th>Description</th> </tr>";
@@ -26,38 +26,33 @@ $(document).ready(function () {
             }
         });
     };
-
     $("#show").click(function () {
         $("#show").hide();
         $("#hide").show();
-        $("#hide").css({display: "block"});
+        $("#hide").css({
+            display: "block"
+        });
     });
-
     $("#cancel").click(function () {
         $("#show").show();
         $("#hide").hide();
     });
-
     $('#searchbutton').on('click', function (e) {
         var $link = $(e.target);
         e.preventDefault();
         if (!$link.data('lockedAt') || +new Date() - $link.data('lockedAt') > 300) {
-            event.preventDefault();
+            e.preventDefault();
             var value = $('#searchText').val();
             getAppointments(value);
         }
         $link.data('lockedAt', +new Date());
     });
-
-
     $('#searchText').keypress(function (event) {
-
         var keycode = (event.keyCode ? event.keyCode : event.which);
         if (keycode == '13') {
             alert("Press With Mouse");
         }
         event.stopPropagation();
     });
-
     getAppointments("");
 });
